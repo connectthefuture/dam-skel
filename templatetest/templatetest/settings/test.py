e1fab5dd9723073479159adaa38a3ccc082dbd1f
johnb <john.bragato@bluefly.com>
2013-12-16 14:30:57 -0500

    cmt

---
+++ templatetest/templatetest/settings/test.py
@@ -0,0 +1,21 @@
+"""
+This is an example settings/test.py file.
+Use this settings file when running tests.
+These settings overrides what's in settings/base.py
+"""
+
+from .base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+
+SECRET_KEY = 'nch0$#19gvp79^09ajg1pau=fpox548qj$w&u_8g60d798%@b+'
